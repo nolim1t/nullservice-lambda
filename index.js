fa825cf9283c2de27e49406ba88857beee5a977d
Barry <barry.teoh@perceptionz.net>
2016-07-15 17:20:20 +0800

    Add placeholdr

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 exports.handler = function(event, context) {
     if (event.apiversion >= 20160715) {
       if (event.action != undefined) {
+        context.succeed(JSON.stringify({"meta": {"code": 400, "msg": "Not implemented yet"}}));
       } else {
         if (event.redirecturl != undefined) {
           // Do redirect
