0e7c1d4cdc1a208b9a512a2ad33a740f3eeda0f9
Barry <barry.teoh@perceptionz.net>
2016-07-16 16:09:28 +0800

    Fix up the function a little

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 exports.handler = function(event, context) {
-    if (event.apiversion >= 20160715) {
+    if (parseInt(event.version) >= 20160715) {
       if (event.action != undefined) {
-        context.succeed(JSON.stringify({"meta": {"code": 400, "msg": "Not implemented yet"}}));
+        context.succeed({"meta": {"code": 400, "msg": "Not implemented yet"}});
       } else {
         if (event.redirecturl != undefined) {
           // Do redirect
           context.fail(event.redirecturl.toString());
         } else {
           // Send parameters as JSON
-          context.succeed(JSON.stringify({"parameters": event}));
+          context.succeed({"parameters": event});
         }
       }
     } else {
-      context.error("Invalid version. apiversion must be over 20160715");
+      context.fail("Invalid version")
     }
 }
